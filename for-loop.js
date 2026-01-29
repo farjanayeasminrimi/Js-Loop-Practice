3e94ef107a19a966b7e72230c7bc36931eb778eb
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-01-29 14:27:58 +0600

    Solved challenges through loop

--- for-loop.js
+++ for-loop.js
@@ -1,81 +1,86 @@
 /*
 "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
 */
+for(let i=1; i <= 60; i++){
+  console.log(i, 'I will invest at least 6hrs every single day for next 60 days!');
+}
 
-// for(let i=1; i <= 60; i++){
-//   console.log(i, 'I will invest at least 6hrs every single day for next 60 days!');
-// }
 
 /*
 Subtask-1:
 Find all the odd numbers from 61 to 100.
 */
-// for(let odd = 61; odd <= 100; odd++){
-//   // odd % 2 == 1 ? console.log(odd): console.log("even");
-//   if(odd % 2 == 1){
-//     console.log(odd);
-//   }
-//   else{
-//     continue;
-//   }
-// }
+for(let odd = 61; odd <= 100; odd++){
+  // odd % 2 == 1 ? console.log(odd): console.log("even");
+  if(odd % 2 == 1){
+    console.log(odd);
+  }
+  else{
+    continue;
+  }
+}
+
 
 /*
 Subtask-2:
 Find all the even numbers from 78 to 98.
 */
-// for(let even = 78; even <= 98; even++){
-//   if(even % 2 == 0){
-//     console.log(even);
-//   }
-//   else{
-//     continue;
-//   }
-// }
+for(let even = 78; even <= 98; even++){
+  if(even % 2 == 0){
+    console.log(even);
+  }
+  else{
+    continue;
+  }
+}
 
 
 /*
 Subtask-1:
 Display sum of all the odd numbers from 91 to 129.
 */
-// let sum = 0;
-// for(let odd = 91; odd <= 129; odd++){
-//   if(odd%2==1){
-//     sum+=odd;
-//   }
-//   else{
-//     continue;
-//   }
-// }
-// console.log(sum);
+let sum = 0;
+for(let odd = 91; odd <= 129; odd++){
+  if(odd%2==1){
+    sum+=odd;
+  }
+  else{
+    continue;
+  }
+}
+console.log(sum);
+
+
 /*
 Subtask-2:
 Display sum of all the even numbers from 51 to 85.
 */
+let evenSum = 0;
+for(let even = 51; even <= 85; even++){
+  if(even%2==0){
+    evenSum += even;
+  }
+  else{
+    continue;
+  }
+}
+console.log(evenSum);
 
-// let evenSum = 0;
-// for(let even = 51; even <= 85; even++){
-//   if(even%2==0){
-//     evenSum += even;
-//   }
-//   else{
-//     continue;
-//   }
-// }
-// console.log(evenSum);
+
 
 /*
 Generate a multiplication table for number 9
 */
-// let table = 9;
-// for(let count = 1; count<= 10; count++){
-//   let multiplication = table * count;
-//   console.log(multiplication);
-// }
+let table = 9;
+for(let count = 1; count<= 10; count++){
+  let multiplication = table * count;
+  console.log(multiplication);
+}
+
 
 /*
 Implement a countdown timer that counts down from 81 to 65.
 */
-for(let count = 81; count >= 65; count--){
-  console.log(count);
+for(let countdown = 81; countdown >= 65; countdown--){
+  console.log(countdown);
 }
--- while-loop.js
+++ while-loop.js
@@ -1,83 +1,87 @@
 /*
 "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
 */
-// let i = 1;
-// while(i <= 60){
-//   console.log('I will invest at least 6 hrs every single day for next 60 days!');
-//   i++;
-// }
+let i = 1;
+while(i <= 60){
+  console.log('I will invest at least 6 hrs every single day for next 60 days!');
+  i++;
+}
+
 
 /*
 Subtask-1:
 Find all the odd numbers from 61 to 100.
 */
-// let odd = 61;
-// while(odd<=100){
-//   if(odd%2==1){
-//     console.log(odd);
-//   }
-//   odd++;
+let odd = 61;
+while(odd<=100){
+  if(odd%2==1){
+    console.log(odd);
+  }
+  odd++;
 
-// }
+}
+
 
 /*
 Subtask-2:
 Find all the even numbers from 78 to 98.
 */
-// let even = 78;
-// while(even <= 98){
-//   if(even%2==0){
-//     console.log(even);
-//   }
-//   even++;
-// }
+let even = 78;
+while(even <= 98){
+  if(even%2==0){
+    console.log(even);
+  }
+  even++;
+}
+
 
 /*
 Subtask-1:
 Display sum of all the odd numbers from 81 to 131.
 */
-// let sum = 0;
-// let oddNum = 81;
-// while (oddNum <= 131) {
-//   if (oddNum % 2 == 1) {
-//     sum += oddNum;
-//   }
-//   oddNum++;
-// }
-// console.log(sum);
+let sum = 0;
+let oddNum = 81;
+while (oddNum <= 131) {
+  if (oddNum % 2 == 1) {
+    sum += oddNum;
+  }
+  oddNum++;
+}
+console.log(sum);
+
+
 /*
 Subtask-2:
 Display sum of all the even numbers from 206 to 311.
 */
+let evenSum = 0;
+let evenNum = 206;
+while(evenNum<=311){
+  if(evenNum%2==0){
+ evenSum+=evenNum;
+}
+  evenNum++;
+}
+console.log(evenSum);
 
-// let evenSum = 0;
-// let evenNum = 206;
-// while(evenNum<=311){
-//   if(evenNum%2==0){
-//  evenSum+=evenNum;
-// }
-//   evenNum++;
-// }
-// console.log(evenSum);
 
 /*
 As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
 */
-// let table = 5;
-// let count = 1;
-// while(count <= 10){
-//   let multiplication = table * count;
-//   console.log(multiplication);
-//   count++;
-// }
-
+let table = 5;
+let count = 1;
+while(count <= 10){
+  let multiplication = table * count;
+  console.log(multiplication);
+  count++;
+}
 
 /*
 Implement a countdown timer that counts down from 21 to 15.
 */
-let count = 21;
-while(count>=15){
-  console.log(count);
-  count--;
+let countDown = 21;
+while(countDown>=15){
+  console.log(countDown);
+  countDown--;
 }
 
