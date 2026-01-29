98ea91086359deb909ac65e84fbdf7a529b0b1b2
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-01-29 14:07:24 +0600

    Solved challenges through loop

---
+++ for-loop.js
@@ -0,0 +1,74 @@
+/*
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+*/
+
+// for(let i=1; i <= 60; i++){
+//   console.log(i, 'I will invest at least 6hrs every single day for next 60 days!');
+// }
+
+/*
+Subtask-1:
+Find all the odd numbers from 61 to 100.
+*/
+// for(let odd = 61; odd <= 100; odd++){
+//   // odd % 2 == 1 ? console.log(odd): console.log("even");
+//   if(odd % 2 == 1){
+//     console.log(odd);
+//   }
+//   else{
+//     continue;
+//   }
+// }
+
+/*
+Subtask-2:
+Find all the even numbers from 78 to 98.
+*/
+// for(let even = 78; even <= 98; even++){
+//   if(even % 2 == 0){
+//     console.log(even);
+//   }
+//   else{
+//     continue;
+//   }
+// }
+
+
+/*
+Subtask-1:
+Display sum of all the odd numbers from 91 to 129.
+*/
+// let sum = 0;
+// for(let odd = 91; odd <= 129; odd++){
+//   if(odd%2==1){
+//     sum+=odd;
+//   }
+//   else{
+//     continue;
+//   }
+// }
+// console.log(sum);
+/*
+Subtask-2:
+Display sum of all the even numbers from 51 to 85.
+*/
+
+// let evenSum = 0;
+// for(let even = 51; even <= 85; even++){
+//   if(even%2==0){
+//     evenSum += even;
+//   }
+//   else{
+//     continue;
+//   }
+// }
+// console.log(evenSum);
+
+/*
+Generate a multiplication table for number 9
+*/
+let table = 9;
+for(let count = 1; count<= 10; count++){
+  let multiplication = table * count;
+  console.log(multiplication);
+}
