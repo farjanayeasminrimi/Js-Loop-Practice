bfb77fe102c4e296a505d42cf35cade15248cd52
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-01-29 21:53:43 +0600

    solved exceeds challenge

--- break.js
+++ break.js
@@ -11,13 +11,24 @@ Write a loop 1 to 200. Use break to exit the loop once you find 100.
 /* 
 Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
 */
-let sum = 0;
-let num = 1;
-while(num < 200){
-  if(sum+num > 100){
+// let sum = 0;
+// let num = 1;
+// while(num < 200){
+//   if(sum+num > 100){
+//     break;
+//   }
+//   sum+=num;
+//   num++;
+// }
+//   console.log(sum);
+
+
+/* 
+Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
+*/
+for(let i =1; i<= 100; i++){
+  if(i>1 && Number.isInteger(Math.sqrt(i))){
     break;
   }
-  sum+=num;
-  num++;
+  console.log(i);
 }
-  console.log(sum);
